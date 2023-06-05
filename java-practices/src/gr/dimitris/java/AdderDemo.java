28dd37197601eb8740b20308d14f70c4eedd4da1
SerDimitris <d.serafeimidis@outlook.com>
2023-06-05 11:22:22 +0300

    New class: AdderDemo

---
+++ java-practices/src/gr/dimitris/java/AdderDemo.java
@@ -0,0 +1,14 @@
+package gr.dimitris.java;
+
+public class AdderDemo {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int sum = 0;
+
+        sum = num1 + num2;
+
+        System.out.printf("The sum of %d and %d equals to %d.", num1, num2, sum);
+    }
+}
