87b0f54cc19d8726a5fc4b4df82db1e3271d5b0c
SerDimitris <d.serafeimidis@outlook.com>
2023-06-20 11:01:44 +0300

    20-6-2023 update

--- BankApp/src/gr/aueb/cf/exceptions/SsnNotValideException.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.exceptions;
-
-public class SsnNotValideException extends Exception{
-    private static final long serialVersionUID = 1L;
-
-    public SsnNotValideException(String ssn){
-        super("Ssn " + ssn + " is not valid");
-    }
-}
